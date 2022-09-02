3c77abde4df7b72e7a2ca7fddfccc7a41c26e59d
enzonjagi <amosndungo@gmail.com>
2022-09-02 15:52:37 +0300

    Adding the projects to GitHub for Portfolio Purposes

---
+++ naive_bayes/nb_author_id.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+""" 
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+
+    Use a Naive Bayes Classifier to identify emails by their authors
+    
+    authors and labels:
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+##############################################################
+# Enter Your Code Here
+
+
+
+def check_for_accuracy(pred):
+    '''Calculating the accuracy'''
+    from sklearn.metrics import accuracy_score
+    accuracy = accuracy_score(labels_test, pred)
+    return accuracy
+
+
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+accuracy = check_for_accuracy(pred=pred)
+print(f'{accuracy}')
+
+
+##############################################################
+
+##############################################################
+'''
+You Will be Required to record time for Training and Predicting 
+The Code Given on Udacity Website is in Python-2
+The Following Code is Python-3 version of the same code
+'''
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+
+t0 = time()
+clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
+
+##############################################################
